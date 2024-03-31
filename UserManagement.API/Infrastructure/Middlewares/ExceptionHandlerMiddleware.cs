be3126d0559bc53a0322ccceb9e8b4a819cf0774
giorgimate <108875518+giorgimate@users.noreply.github.com>
2024-03-31 13:42:29 +0400

    add exceptions loger

--- UserManagement.API/Infrastructure/Middlewares/ExceptionHandlerMiddleware.cs
+++ UserManagement.API/Infrastructure/Middlewares/ExceptionHandlerMiddleware.cs
@@ -28,11 +28,23 @@ namespace UserManagement.API.Infrastructure.Middlewares
             var error = new ApiError(context, ex);
             var result = JsonConvert.SerializeObject(error);
 
+            await LogExceptionToFile(ex);
+
             context.Response.Clear();
             context.Response.ContentType = "application/json";
             context.Response.StatusCode = error.Status.Value;
 
             await context.Response.WriteAsync(result);
+        }
+        private async Task LogExceptionToFile(Exception ex)
+        {
+            var exceptionLog = $"{Environment.NewLine}Logged from Middleware {Environment.NewLine}" +
+                               $"Exception Type: {ex.GetType().Name}{Environment.NewLine}" +
+                               $"Message: {ex.Message}{Environment.NewLine}" +
+                               $"StackTrace: {ex.StackTrace}{Environment.NewLine}" +
+                               $"Time: {DateTime.Now}{Environment.NewLine}";
+
+            await File.AppendAllTextAsync("Exceptions.txt", exceptionLog);
         }
     }
 }
